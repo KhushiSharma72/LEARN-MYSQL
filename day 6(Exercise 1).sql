8d0273f6dbd3fec484cfe04a053148be3ec0343f
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2025-06-29 14:27:33 +0530

    Add files via upload

---
+++ day 6(Exercise 1).sql
@@ -0,0 +1,37 @@
+-- create database bank_db;
+-- use bank_db;
+/*create table employee(
+emp_id INT PRIMARY KEY AUTO_INCREMENT,
+name Varchar(50) NOT NULL,
+design VARCHAR(50) NOT NULL DEFAULT "Probation",
+dept VARCHAR(50) NOT NULL);*/
+
+-- desc employee;
+
+/*Insert into employee(emp_id,name,dept,design)
+VALUES (101,"Raju","Loan","Manager")
+*/
+-- select * from employee;
+
+/*Values("Sham","Casher","Cash"),("Paul","Associate","Loan"),("Alex","Accountant","Account"),("Victor","Accountant","Deposite")*/
+
+-- select * from employee;
+
+-- EXERCISE 2
+
+-- Select * from employee where dept="Loan";
+-- select * from employee where emp_id=101;
+-- select emp_id,name from employee where emp_id=101;
+
+/*Update employee
+set dept="IT"
+where name="Paul";*/
+
+/*Delete from employee 
+where name="Sham";*/
+
+
+
+
+
+
