670ff9b85493e3b8f8427125dc885cbc62c919de
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2025-06-29 14:27:05 +0530

    Add files via upload

---
+++ day 5 (ALIAS).sql
@@ -0,0 +1,5 @@
+-- use school_db;
+-- CHANGE THE COLUMN NAME
+-- select acc_no as "Account_NO" from customer4;
+-- CHANGE MULTIPLE COLUMNS
+select acc_no  as "ACCOUNT NO",name as "CUSTOMER NAME" FROM customer4;
