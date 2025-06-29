6a1f79f82e26a7586dab67fc5c458845bf66bea7
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2025-06-29 14:25:11 +0530

    Add files via upload

---
+++ DAY 1.sql
@@ -0,0 +1,39 @@
+-- show databases;
+-- create database store_db;
+-- select database();
+/*create table customers(
+id INT,
+name VARCHAR(50)
+);*/
+
+-- desc customers;
+
+-- use school_db;
+
+/*insert into students(id,name,class)
+VALUES (1,"Roni",5),(2,"Khushi",5),(3,"Sakshi",5),(4,"Mehak",5),(5,"Moni",5);*/
+
+
+--  select* from students;    
+
+-- select name from students;
+
+-- select name,id from students;
+
+-- select * from students where id=2;
+
+/*UPDATE students
+SET class=10
+where name="Sakshi";*/
+
+/* from students
+where name="Mohit";*/
+
+-- Drop table students;
+
+select * from students;
+
+
+
+
+
