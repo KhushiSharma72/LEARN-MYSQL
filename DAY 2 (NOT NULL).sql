d04ab957426223ef61043c34dc08922f31bc02e4
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2025-06-29 14:25:36 +0530

    Add files via upload

---
+++ DAY 2 (NOT NULL).sql
@@ -0,0 +1,27 @@
+-- NOT NULL
+/*USE store_db;
+show tables;*/
+
+-- desc customers;
+
+/*INSERT INTO customers(id,name)
+Values(101,"Khushi");*/
+
+/*Insert into customers(id)
+Values(102);
+select * from customers;*/
+
+-- CASE 2
+/*INSERT into customers
+values(NULL,NULL);
+select* from customers;*/
+
+-- SO TO AVOID NULL WE USE NOT NULL
+/*create table customer1(
+id INT NOT NULL,
+name VARCHAR(50) NOT NULL);*/
+
+desc customer1;
+
+
+
