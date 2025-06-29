6b78d3218ec4b13f66f5b0b4bfa3bc3fe0bbe551
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2025-06-29 14:26:15 +0530

    Add files via upload

---
+++ DAY 4 (Primary key ,Auto Increment).sql
@@ -0,0 +1,13 @@
+-- use school_db;
+/*create Table customer3(
+acc_no INT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+acc_type VARCHAR(100) NOT NULL DEFAULT "Savings"
+);*/
+
+-- desc customer3;
+
+/*INSERT INTO customer3(acc_no,name)
+VALUES(101,"Khushi"),(102,"Mohit");*/
+
+select * from customer3;
