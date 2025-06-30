e267e16c1995b000a5e0508826b3898b7db41643
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2025-06-30 22:59:50 +0530

    Add files via upload

---
+++ DAY8(Sting Function 2).sql
@@ -0,0 +1,33 @@
+-- use bank_db;
+-- SUBSTRING()
+-- select SUBSTRING('hlo sharma',1,3);
+-- select SUBSTRING("HLO BUDDY",5,9);
+-- SELECT SUBSTRING("Hlo bro",-4)
+
+-- select SUBSTRING(emp_id,2)as EMP_ID ,fname from employee1;
+
+-- REPLACE()
+-- select REPLACE("HELLO BUDDY","HELLO","HEY");
+-- select REPLACE("hello bunny","bunny","sunny");
+-- select REPLACE(emp_id,10,100) from employee1;
+-- select replace(emp_id,10,"EMP") from employee1;
+
+-- REVERSE()
+-- select Reverse("khushi");
+-- select REVERSE("Sakshi");
+-- select reverse(fname) as newname from employee1;
+
+-- LOWER AND UPPER()
+-- select UPPER("hello");
+-- select LOWER("HELLO");
+-- select upper(fname) as uppername from employee1;
+-- select LOWER(fname) as newname from employee1;
+
+-- CHAR_LENGTH()
+-- select char_length("khushi");
+-- select char_length("khushi sharma");
+-- select char_length(fname) as Length from employee1;
+
+
+
+
