d28624f75fd117976a4c962e7bb4b8417067371d
KhushiSharma72 <149526448+KhushiSharma72@users.noreply.github.com>
2025-07-04 10:51:23 +0530

    Add files via upload

---
+++ Day9  (String Function 3).sql
@@ -0,0 +1,12 @@
+-- INSERT
+-- select insert('hello friend',5,0,'my ');
+
+-- LEFT RIGHT
+-- select left('hello hey ',3);
+-- select right('hello mam',3);
+
+-- REPEAT
+-- select repeat("hlo",10);
+
+-- Trim
+select trim(  "hlo"    );
